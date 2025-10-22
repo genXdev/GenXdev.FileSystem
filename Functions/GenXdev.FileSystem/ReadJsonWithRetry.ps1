10ff4053753a3b8de5541ae2631e4e46f03af001
René Vaessen <rene.vaessen@gmail.com>
2025-10-23 00:25:02 +0200

    Release 1.308.2025

--- Functions/GenXdev.FileSystem/ReadJsonWithRetry.ps1
+++ Functions/GenXdev.FileSystem/ReadJsonWithRetry.ps1
@@ -2,7 +2,7 @@
 Part of PowerShell module : GenXdev.FileSystem
 Original cmdlet filename  : ReadJsonWithRetry.ps1
 Original author           : René Vaessen / GenXdev
-Version                   : 1.304.2025
+Version                   : 1.308.2025
 ################################################################################
 Copyright (c)  René Vaessen / GenXdev
 
